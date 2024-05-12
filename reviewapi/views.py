bd7a3e744dd5ba9472caa4fe1f69ecfa6659db82
Gaurav Kankotiya <gauravkankotiya@gmail.com>
2024-05-12 18:11:15 +0530

    UI-fix

--- reviewapi/views.py
+++ reviewapi/views.py
@@ -31,23 +31,23 @@ def resume_review(request):
         pdf_file = request.FILES["resumeFile"]
         if pdf_file.name == "":
             return JsonResponse({"error": "No selected file"})
-
-        if pdf_file and pdf_file.name.endswith(".pdf"):
-            resume_text = extract_text_from_pdf(pdf_file)
-            # print(resume_text)
-            review_text = f'''I'm providing you the extracted texts from resume in double quotes and i want you to review this resume carefully and get me the proper review, anylyze the skillset, position, headline, Education and also the preivious experiences and projects and see whether it is align with the skill set and position give the review based on these criterias give me the reiview in just two point one should be 'Overall rating' which is out of 10 and second one should be 'Improvements' which is 3 points for improvements in brief also saperate both 'Overall rating' and 'Improvements' with '---',
-            "{resume_text}"'''
-            review = review_resume(review_text)
-            saprated_text = review.split("---")
-            score = saprated_text[0].split("Overall Rating: ")[-1]
-            points = saprated_text[1]
-            points = [
-                i for i in points.split("\n")[1:5] if i not in ["\n", "Improvements:"]
-            ]
-            return JsonResponse({"score": score, "points": points})
-        else:
-            return JsonResponse(
-                {"error": "Invalid file format. Only PDF files are supported."}
-            )
+        try:
+            if pdf_file and pdf_file.name.endswith(".pdf"):
+                resume_text = extract_text_from_pdf(pdf_file)
+                # print(resume_text)
+                review_text = f'''Suppose You are profession resume reviewer and I'm providing you the extracted texts from resume in double quotes and i want you to review this resume carefully and get me the proper review don't be biased towards positive review, anylyze the skillset, position, headline, Education and also the preivious experiences and projects and see whether it is align with the skill set and position give the review based on these criterias give me the reiview in just two point one should be 'Overall rating' which is out of 10 and second one should be 'Improvements' which is 3 points for improvements in brief,
+                "{resume_text}"'''
+                review = review_resume(review_text)
+                saprated_text = review.split("\n")
+                score = saprated_text[0].split("Overall Rating: ")[-1]
+                points = saprated_text[1:]
+                points = [i for i in points[1:5] if i not in ["\n", "Improvements:"]]
+                return JsonResponse({"score": score, "points": points})
+            else:
+                return JsonResponse(
+                    {"error": "Invalid file format. Only PDF files are supported."}
+                )
+        except:
+            return JsonResponse({"error": "Internal Server Error"})
     else:
         return JsonResponse({"error": "Invalid Request"})
